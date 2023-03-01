c05e51c74ebad4914c949247bbfbddc931905134
DenPC <zmaev.vsu@gmail.com>
2023-03-01 12:46:01 +0300

    Change class to data class

--- mobile/src/main/java/ru/vsu/cs/zmaev/diary/model/Note.kt
+++ mobile/src/main/java/ru/vsu/cs/zmaev/diary/model/Note.kt
@@ -2,26 +2,36 @@ package ru.vsu.cs.zmaev.diary.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import android.os.Parcelable.Creator
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-class Note() : Parcelable {
-
+data class Note (
     @PrimaryKey(autoGenerate = true)
-    val uid: Int = 0
+    var uid: Int = 0,
 
     @ColumnInfo(name = "text")
-    var text: String = ""
+    var text: String? = null,
 
     @ColumnInfo(name = "timestamp")
-    var timestamp: Long = 0
+    var timestamp: Long = 0,
 
     @ColumnInfo(name = "done")
-    var done: Boolean = false
-
-    constructor(parcel: Parcel) : this()
+    var done: Boolean = false,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readLong(),
+        parcel.readByte() != 0.toByte()
+    ) {
+        uid = parcel.readInt()
+        text = parcel.readString()
+        timestamp = parcel.readLong()
+        done = parcel.readByte() != 0.toByte()
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -39,7 +49,7 @@ class Note() : Parcelable {
 
     override fun hashCode(): Int {
         var result = uid
-        result = 31 * result + text.hashCode()
+        result = 31 * result + (text?.hashCode() ?: 0)
         result = 31 * result + timestamp.hashCode()
         result = 31 * result + done.hashCode()
         return result
@@ -49,14 +59,14 @@ class Note() : Parcelable {
         parcel.writeInt(uid)
         parcel.writeString(text)
         parcel.writeLong(timestamp)
-        parcel.writeByte((if (done) 1 else 0).toByte())
+        parcel.writeByte(if (done) 1 else 0)
     }
 
     override fun describeContents(): Int {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<Note> {
+    companion object CREATOR : Creator<Note> {
         override fun createFromParcel(parcel: Parcel): Note {
             return Note(parcel)
         }
